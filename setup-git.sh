f0d6567ac90dd9202ff977971d9e1aa946f9ebb5
wrobbins <27835+wrobbins@users.noreply.github.com>
2024-12-02 17:55:47 -0700

    updates

--- setup-git.sh
+++ setup-git.sh
@@ -2,3 +2,12 @@
 
 git config --global user.email "27835+wrobbins@users.noreply.github.com"
 git config --global user.name "wrobbins"
+
+git config --global core.pager delta
+git config --global interactive.diffFilter 'delta --color-only'
+git config --global merge.conflictStyle zdiff3
+
+git config --global delta.navigate true
+git config --global delta.dark true
+git config --global delta.side-by-side true
+git config --global delta.line-numbers true
